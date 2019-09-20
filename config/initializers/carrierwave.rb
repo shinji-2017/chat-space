bd9466266dbca6d4282cb78debe72767c07c984d
shinji-2017 <shinji2017@icloud.com>
2019-09-20 19:26:51 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'shinji2017'
-  config.asset_host = 'https://ap-northeast-1.amazonaws.com/shinji2017'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/shinji2017'
 end
